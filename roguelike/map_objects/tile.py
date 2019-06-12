4e90dc2d3a91c46aa691b780b909ee5793138b9b
Martin Uribe <clamytoe@gmail.com>
2019-06-12 18:28:14 -0500

    Added a Tile class object

---
+++ roguelike/map_objects/tile.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Union
+
+
+@dataclass
+class Tile:
+    """
+    A tile on a map.
+
+    It may or may not be blocked, and may or may not block sight.
+    """
+
+    blocked: bool
+    block_sight: Union[bool, None]
+
+    def __post_init__(self):
+        self.block_sight = (
+            self.blocked if self.block_sight is None else self.block_sight
+        )
