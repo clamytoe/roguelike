58240b18b26232fa1661be8ca3ddcd2455a93c24
Martin Uribe <clamytoe@gmail.com>
2019-06-12 19:04:57 -0500

    Modified the render function to render the GameMap object

--- roguelike/render_functions.py
+++ roguelike/render_functions.py
@@ -1,15 +1,28 @@
 import tcod
 
 
-def render_all(con, entities, screen_width, screen_height):
+def render_all(con, entities, game_map, screen_width, screen_height, colors):
     """
     Dra all entities in the list
     :param con: Console window to draw on
     :param entities: List of entities to draw
+    :param game_map: GameMap object
     :param screen_width: Width of the screen
     :param screen_height: Height of the screen
+    :param colors: GameMap color values
     :return: None
     """
+    # Draw all the tiles in the game map
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
+
+            if wall:
+                tcod.console_set_char_background(con, x, y, colors.get("dark_wall"), tcod.BKGND_SET)
+            else:
+                tcod.console_set_char_background(con, x, y, colors.get("dark_ground"), tcod.BKGND_SET)
+
+    # Draw all entities in the list
     for entity in entities:
         draw_entity(con, entity)
 
