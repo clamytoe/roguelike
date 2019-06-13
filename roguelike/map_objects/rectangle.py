0bee2d86591c0158566f98a5133323d2f73b7d74
Martin Uribe <clamytoe@gmail.com>
2019-06-12 20:25:02 -0500

    Added methods for getting the center of a room and checking if two rooms intersect to the Rect class

--- roguelike/map_objects/rectangle.py
+++ roguelike/map_objects/rectangle.py
@@ -11,3 +11,13 @@ class Rect:
     def __post_init__(self):
         self.x2 = self.x1 + self.w
         self.y2 = self.y1 + self.h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return center_x, center_y
+
+    def intersect(self, other):
+        # returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
