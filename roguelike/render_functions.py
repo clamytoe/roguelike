0f85639c8d4199f7bc6891e6ebd6f14416555c67
Martin Uribe <clamytoe@gmail.com>
2019-06-13 19:30:17 -0500

    Modified the render code that it would take into account the fov of the player

--- roguelike/render_functions.py
+++ roguelike/render_functions.py
@@ -1,36 +1,54 @@
 import tcod
 
 
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
+def render_all(
+    con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors
+):
     """
     Dra all entities in the list
     :param con: Console window to draw on
     :param entities: List of entities to draw
     :param game_map: GameMap object
+    :param fov_map: Field of View map
+    :param fov_recompute: Boolean flag to determine if FOV should be recomputed
     :param screen_width: Width of the screen
     :param screen_height: Height of the screen
     :param colors: GameMap color values
     :return: None
     """
     # Draw all the tiles in the game map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
+    if fov_recompute:
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = tcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
 
-            if wall:
-                tcod.console_set_char_background(
-                    con, x, y, colors.get("dark_wall"), tcod.BKGND_SET
-                )
-            else:
-                tcod.console_set_char_background(
-                    con, x, y, colors.get("dark_ground"), tcod.BKGND_SET
-                )
+                if visible:
+                    if wall:
+                        tcod.console_set_char_background(
+                            con, x, y, colors.get("light_wall"), tcod.BKGND_SET
+                        )
+                    else:
+                        tcod.console_set_char_background(
+                            con, x, y, colors.get("light_ground"), tcod.BKGND_SET
+                        )
+
+                    game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:
+                    if wall:
+                        tcod.console_set_char_background(
+                            con, x, y, colors.get("dark_wall"), tcod.BKGND_SET
+                        )
+                    else:
+                        tcod.console_set_char_background(
+                            con, x, y, colors.get("dark_ground"), tcod.BKGND_SET
+                        )
 
     # Draw all entities in the list
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
 
-    tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+    tcod.console_blit(con, 0, 0, screen_width, screen_height, con, 0, 0)
 
 
 def clear_all(con, entities):
@@ -44,15 +62,17 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
+def draw_entity(con, entity, fov_map):
     """
     Draw an entity on the screen
     :param con: Console window
     :param entity: Entity object to draw
+    :param fov_map: Field of View map
     :return:
     """
-    tcod.console_set_default_foreground(con, entity.color)
-    tcod.console_put_char(con, entity.x, entity.y, entity.char, tcod.BKGND_NONE)
+    if tcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        tcod.console_set_default_foreground(con, entity.color)
+        tcod.console_put_char(con, entity.x, entity.y, entity.char, tcod.BKGND_NONE)
 
 
 def clear_entity(con, entity):
