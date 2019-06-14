c9eba1878fbd3694c40745ce40252bd61f2bab3f
Martin Uribe <clamytoe@gmail.com>
2019-06-13 19:32:32 -0500

    Modified the map to use fov code

--- roguelike/app.py
+++ roguelike/app.py
@@ -10,6 +10,7 @@ import tcod
 import tcod.event
 
 from .entity import Entity
+from .fov_functions import initialize_fov, recompute_fov
 from .input_handlers import handle_keys
 from .map_objects.game_map import GameMap
 from .render_functions import clear_all, clear_entity, draw_entity, render_all
@@ -27,7 +28,15 @@ SCREEN_HEIGHT = 50
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
-COLORS = {"dark_wall": tcod.Color(0, 0, 100), "dark_ground": tcod.Color(50, 50, 150)}
+FOV_ALGORITHM = 0
+FOV_LIGHT_WALLS = True
+FOV_RADIUS = 10
+COLORS = {
+    "dark_wall": tcod.Color(0, 0, 100),
+    "dark_ground": tcod.Color(50, 50, 150),
+    "light_wall": tcod.Color(130, 110, 50),
+    "light_ground": tcod.Color(200, 180, 50),
+}
 
 
 def main():
@@ -48,30 +57,50 @@ def main():
         game_map.make_map(
             MAX_ROOMS, ROOM_MIN_SIZE, ROOM_MAX_SIZE, MAP_WIDTH, MAP_HEIGHT, player
         )
+        fov_recompute = True
+        fov_map = initialize_fov(game_map)
 
         while not tcod.console_is_window_closed():
             tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
-            render_all(con, entities, game_map, SCREEN_WIDTH, SCREEN_HEIGHT, COLORS)
 
+            if fov_recompute:
+                recompute_fov(
+                    fov_map,
+                    player.x,
+                    player.y,
+                    FOV_RADIUS,
+                    FOV_LIGHT_WALLS,
+                    FOV_ALGORITHM,
+                )
+
+            render_all(
+                con,
+                entities,
+                game_map,
+                fov_map,
+                fov_recompute,
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                COLORS,
+            )
+            fov_recompute = False
             tcod.console_flush()
-
             clear_all(con, entities)
-
             action = handle_keys(key)
-
             move = action.get("move")
-            exit = action.get("exit")
-            fullscreen = action.get("fullscreen")
+            exit_game = action.get("exit")
+            full_screen = action.get("full_screen")
 
             if move:
                 dx, dy = move
                 if not game_map.is_blocked(player.x + dx, player.y + dy):
                     player.move(dx, dy)
+                    fov_recompute = True
 
-            if exit:
+            if exit_game:
                 return True
 
-            if fullscreen:
+            if full_screen:
                 tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
 
 
